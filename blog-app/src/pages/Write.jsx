933709a08d02838a48fce5d4741ecd3cd03880ce
holyrocking <zggshsgsgsh@gmail.com>
2025-09-18 22:48:13 +0300

    improve the write page

--- blog-app/src/pages/Write.jsx
+++ blog-app/src/pages/Write.jsx
@@ -4,7 +4,7 @@ import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
   const [value, setValue] = React.useState('');
-  console.log(value);
+ 
   return (
     <div className='add'>
       <div className="content">
@@ -14,8 +14,36 @@ const Write = () => {
         </div>
       </div>
         <div className="menu">
-          <div className="item">i1</div>
-          <div className="item">i2</div>
+          <div className="item">
+            <h1>Publish</h1>
+            <span>
+              <b>Status: </b>Draft
+            </span>
+             <span>
+              <b>Visibility: </b>Public
+            </span>
+            <input type="file" name='' id='file' />
+            <label className='file'  htmlFor="file">Upload Image</label>
+            <div className="buttons">
+              <button>Save as a draft</button>
+              <button>Update</button>
+            </div>
+          </div>
+          <div className="item">
+            <h1>Category</h1>
+            <input type="radio" name="cat" value="art" id='art' />
+            <label htmlFor="art">Art</label>
+            <input type="radio" name="cat" value="science" id='science' />
+            <label htmlFor="science">Science</label>
+            <input type="radio" name="cat" value="technology" id='technology' />
+            <label htmlFor="technology">Technology</label>
+            <input type="radio" name="cat" value="cinema" id='cinema' />
+            <label htmlFor="cinema">Cinema</label>
+            <input type="radio" name="cat" value="design" id='design' />
+            <label htmlFor="design">Design</label>
+            <input type="radio" name="cat" value="food" id='food' />
+            <label htmlFor="food">Food</label>
+          </div>
         </div>
       
     </div>
